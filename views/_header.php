3a12f7e8e7b5866fa456a6241f0bdea5eea3f2d0
Paolo Mazzacani <paolo@modiglianiarchives.com>
2026-05-11 18:14:06 +0200

    header: cache-bust style.css via filemtime query so browsers always fetch fresh

--- views/_header.php
+++ views/_header.php
@@ -7,7 +7,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,500;1,400;1,500&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="/assets/style.css">
+<link rel="stylesheet" href="/assets/style.css?v=<?= @filemtime(__DIR__ . '/../assets/style.css') ?: '1' ?>">
 <link rel="icon" type="image/svg+xml" href="/assets/logo.svg">
 </head>
 <body>
