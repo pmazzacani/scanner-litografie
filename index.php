9829b22a6388e33d3e4d53ddc4281cdd10973b36
Paolo Mazzacani <paolo@modiglianiarchives.com>
2026-05-11 17:17:24 +0200

    index: send no-store on certificate pages so SiteGround edge cache doesn't serve stale data

--- index.php
+++ index.php
@@ -14,6 +14,9 @@ if ($microId === null) {
     exit;
 }
 
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+
 $res = fetch_certificate($microId);
 
 if (!$res['ok']) {
